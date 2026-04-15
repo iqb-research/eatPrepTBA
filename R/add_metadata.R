9f2bc1ffc1a60ed1d5511e186aefd4caf8c24175
Edna Gwendolyn Daphne Grewers <grewered@user.hu-berlin.de>
2026-04-15 16:12:17 +0200

    added assertions for the tibble column names for some functions

--- R/add_metadata.R
+++ R/add_metadata.R
@@ -10,7 +10,9 @@
 add_metadata <- function(units) {
   cli_setting()
   # input validation
+  units_cols <- c("unit_profiles", "items_profiles")
   checkmate::assert_tibble(units)
+  if(!(all(units_cols %in% colnames(units)))) stop(paste0("'units' must contain the columns {", paste0(units_cols, collapse = ", "), "}, but is missing the column(s): {", paste0(setdiff(units_cols, colnames(units)), collapse = ", "), "}."))
 
   # Conserve attributes
   unit_attributes <- attributes(units)
